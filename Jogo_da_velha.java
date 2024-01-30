9696b35d16e490690a64f275c657e2440ee0c6b4
“LucasFranczak” <lucasfranczak@gmail.com>
2024-01-30 17:04:21 -0300

    inicio do jogo

--- Jogo_da_velha.java
+++ Jogo_da_velha.java
@@ -14,8 +14,16 @@ public class Jogo_da_velha {
             desenhaJogo(velha);
             vitoria=verificaVitoria((velha));
 
-            if(!vitoria.equals(""){
-                System.out.printf("Jogador %s venceu%n",)
+            if(!vitoria.equals("")){
+                System.out.printf("Jogador %s venceu%n",vitoria);
+                break;
+            }
+            try{
+
+
+
+            } catch ( Exception e){
+                System.out.println("'ERRO!!!'");
             }
 
         }
@@ -23,7 +31,7 @@ public class Jogo_da_velha {
     }
 
     public static void desenhaJogo(Campo[][] velha){ // redesenhar a cada interaçao
-        // limpar tela
+        limparTela();
         System.out.print (     "    0    1    2 /n");
         System.out.println (   "    _ _  _ _  _ _ ");
         System.out.printf("0   %c | %c | %c %n",velha[0][0].getsimbolo(), velha[0][1].getsimbolo(), velha[0][2].getsimbolo());
@@ -33,8 +41,14 @@ public class Jogo_da_velha {
         System.out.printf("2   %c | %c | %c %n",velha[2][0].getsimbolo(), velha[2][1].getsimbolo(), velha[2][2].getsimbolo());
     }
 
+    public static void limparTela(){
+         for (int cont = 0; cont<200; cont ++){ 
+            System.out.println(""); 
+        }
+    }
+
     public static String verificaVitoria(Campo[][] velha){
-        return "0";
+        return "";
     }
 
 }
