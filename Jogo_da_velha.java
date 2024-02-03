5c72c6861cadb26194761e13ece6aac810d05d32
“LucasFranczak” <lucasfranczak@gmail.com>
2024-02-03 14:30:52 -0300

    Jogo concluido

--- Jogo_da_velha.java
+++ Jogo_da_velha.java
@@ -10,6 +10,8 @@ public class Jogo_da_velha {
         String vitoria="";
         Scanner scan =new Scanner(System.in);
         
+        iniciarJogo(velha);
+
         while (game) {
             desenhaJogo(velha);
             vitoria=verificaVitoria((velha));
@@ -19,7 +21,13 @@ public class Jogo_da_velha {
                 break;
             }
             try{
-
+                if (verificarJogada(velha, jogar(scan, simboloAtual), simboloAtual)){
+                    if (simboloAtual == 'X'){
+                        simboloAtual = 'O';
+                    } else {
+                        simboloAtual='X';
+                    }
+                }
 
 
             } catch ( Exception e){
@@ -32,13 +40,13 @@ public class Jogo_da_velha {
 
     public static void desenhaJogo(Campo[][] velha){ // redesenhar a cada interaçao
         limparTela();
-        System.out.print (     "    0    1    2 /n");
+        System.out.println (     "    0    1    2 ");
+        System.out.printf("0   %c  | %c  | %c %n",velha[0][0].getsimbolo(), velha[0][1].getsimbolo(), velha[0][2].getsimbolo());
         System.out.println (   "    _ _  _ _  _ _ ");
-        System.out.printf("0   %c | %c | %c %n",velha[0][0].getsimbolo(), velha[0][1].getsimbolo(), velha[0][2].getsimbolo());
+        System.out.printf("1   %c  | %c  | %c %n",velha[1][0].getsimbolo(), velha[1][1].getsimbolo(), velha[1][2].getsimbolo());
         System.out.println (   "    _ _  _ _  _ _ ");
-        System.out.printf("1   %c | %c | %c %n",velha[1][0].getsimbolo(), velha[1][1].getsimbolo(), velha[1][2].getsimbolo());
-        System.out.println (   "    _ _  _ _  _ _ ");
-        System.out.printf("2   %c | %c | %c %n",velha[2][0].getsimbolo(), velha[2][1].getsimbolo(), velha[2][2].getsimbolo());
+        System.out.printf("2   %c  | %c  | %c %n",velha[2][0].getsimbolo(), velha[2][1].getsimbolo(), velha[2][2].getsimbolo());
+        System.out.println();
     }
 
     public static void limparTela(){
@@ -48,7 +56,7 @@ public class Jogo_da_velha {
     }
     public static int[] jogar (Scanner scan, char sa){
         int p[]=new int[2];
-        System.out.printf("%s %c%n","Quem Joga: ", sa)
+        System.out.printf("%s %c%n","Quem Joga: ", sa);
         System.out.print("Informa a linha: ");
         p[0]=scan.nextInt();
         System.out.print("Informa a coluna: ");
@@ -58,12 +66,20 @@ public class Jogo_da_velha {
 
     }
 
-    public Boolean verificarJogada(Campo[][] velha, int p[], simboloAtual){
+    public static Boolean verificarJogada(Campo velha[][], int p[], char simboloAtual){
         if (velha[p[0]][p[1]].getsimbolo()== ' '){
             velha[p[0]][p[1]].setsimbolo(simboloAtual);
             return true;
         } else{
             return false;
+        }
+    }
+    
+    public static void iniciarJogo(Campo[][] velha){
+        for(int l=0 ; l<3;l++){
+            for (int c=0;c<3;c++){
+                velha[l][c] = new Campo();
+            }
         }
     }
 
